7938704684064a2a3ebe1cfeef057cf578cdeb59
MasonGallo <MasonGallo@users.noreply.github.com>
2016-06-01 16:09:11 -0400

    Add rough draft implementation of generate and plot

--- R/hyperparameterValidation.R
+++ R/hyperparameterValidation.R
@@ -1,26 +1,24 @@
-#' Generate validation data from a tuning result.
+#' Generate hyperparameter validation data from a tuning result.
 #'
 #' @description 
-#' Generate cleaned validation from a tuning result. The object returned
-#' can be used for custom visualization or passed downstream to an out of the
-#' box mlr method, \code{\link{plotValidationData}}.
+#' Generate cleaned hyperparameter validation from a tuning result. The object 
+#' returned can be used for custom visualization or passed downstream to an out 
+#' of the box mlr method, \code{\link{plotValidation}}.
 #' @param TuneResult \code{\link{TuneResult}} & \code{\link{OptResult}}\cr
 #'  The tuning results, also containing the optimizer results
-#' @param include.diagnostics [\code{logical(1)}]\cr
+#' @param include.diagnostics \code{logical(1)}\cr
 #'  Should diagnostic info (eol and error msg) be included?
 #'
-#' @return \code{\link{ValidationData}}
-#'  Object containing the validation dataframe, the tuning performance measures
+#' @return \code{ValidationData}
+#'  object containing the validation dataframe, the tuning performance measures
 #'  used, the hyperparameters used, a flag for including diagnostic info, and 
 #'  the optimization algorithm used.
 #'
 #' @examples
-#' \dontrun{
 #' res = tuneParams("classif.ksvm", task = pid.task, resampling = rdesc,
 #' par.set = ps, control = ctrl)
 #' valid = generateValidationData(res)
 #' valid$data
-#' }
 #' @export
 generateValidationData = function(TuneResult, include.diagnostics = FALSE) {
   checkmate::assertClass(TuneResult, classes = c("TuneResult", "OptResult"))
@@ -52,25 +50,121 @@ print.ValidationData = function(x, ...) {
   catf("Hyperparameters: %s", collapse(x$hyperparams))
   catf("Measures: %s", collapse(x$measures))
   catf("Optimizer: %s", collapse(x$optimization))
-  print("Snapshot of data:")
+  catf("Snapshot of $data:")
   print(head(x$data))
 }
 
+#' Plot the hyperparameter validation data
+#' 
+#' @description 
+#' Plot hyperparameter validation path. Automated plotting method for 
+#' \code{ValidationData} object. Useful for determining the importance
+#' or effect of a particular hyperparameter on some performance measure and/or
+#' optimizer.
+#'
+#' @param ValidationData \code{ValidationData}\cr
+#' @param x.axis \code{character(1)}\cr
+#'  Specify what should be plotted on the x axis. Must be a column from
+#'  \code{ValidationData$data}
+#' @param y.axis \code{character(1)}\cr
+#'  Specify what should be plotted on the y axis. Must be a column from
+#'  \code{ValidationData$data}
+#' @param plot.line \code{logical(1)}\cr
+#'  If TRUE, will connect plotted points with a line
+#' @param local.optima \code{logical(1)}\cr
+#'  If TRUE, will color found local optima points green
+#' @param facet \code{character(1)}\cr
+#'  TODO: not yet implemented
+#' @param pretty.names \code{logical(1)}\cr
+#'  Whether to use the \code{\link{Measure}} name instead of the id in the plot
+#' @param title \code{character(1)}\cr
+#'  A custom title for the returned plot
+#'
+#' @return
+#'  ggplot2 plot object
+#' @export
+#'
+#' @examples
+#' ps = makeParamSet(makeDiscreteParam("C", values = 2^(-4:4)))
+#' ctrl = makeTuneControlRandom()
+#' rdesc = makeResampleDesc("CV", iters = 3L)
+#' res = tuneParams("classif.ksvm", task = pid.task, resampling = rdesc,
+#' par.set = ps, control = ctrl, measures = acc)
+#' valid = generateValidationData(res)
+#' plotValidation(valid, x.axis = "C", y.axis = "acc.test.mean")
 plotValidation = function(ValidationData, x.axis = NULL, y.axis = NULL, 
-                          plot.type = "scatter", local.optima = TRUE, 
+                          plot.line = TRUE, local.optima = FALSE, 
                           facet = NULL, pretty.names = TRUE, title = NULL) {
   checkmate::assertClass(ValidationData, classes = "ValidationData")
   if (is.null(x.axis) || is.null(y.axis))
     stopf("x.axis and y.axis must both be specified!")
   # TODO later once functionality implemented
-  if (length(x.axis) > 1 || length(y.axis) > 1)
-    stopf("Greater than 1 length x.axis or y.axis not yet supported")
   if (!is.null(facet))
     stopf("Facet argument is not yet supported")
-  
-  
+  if (length(x.axis) > 1 || length(y.axis) > 1)
+    stopf("Greater than 1 length x.axis or y.axis not yet supported")
   
   d = ValidationData$data
+  d_names = names(d)
+  if (!(x.axis %in% d_names) || !(y.axis %in% d_names))
+    stopf("x.axis and y.axis must both match column names in the data!")
   
+  # save for later formatting checks
+  x_measure = x.axis %in% ValidationData$measures
+  y_measure = y.axis %in% ValidationData$measures
   
+  plt = ggplot(d, aes_string(x = x.axis, y = y.axis))
+  
+  if (plot.line) 
+    plt = plt + geom_line()
+  
+  if (local.optima){
+    # did we even ask to plot a measure so local optima matters?
+    measure_used = 0
+    if (x_measure) {
+      measure_used = measure_used + 1
+      measure_axis = x.axis
+    }
+    if (y_measure) {
+      measure_used = measure_used + 1
+      if (measure_used > 1) {
+        print("Local optima for multiple measures not yet supported!")
+      } else {
+        measure_axis = y.axis
+      }
+    } 
+    if (measure_used == 1) {
+      d$local_optima = FALSE
+      # do we care about local min or max?
+      if (eval(as.name(stri_split_fixed(measure_axis, 
+                                        ".test.mean")[[1]][1]))$minimize) {
+        # find local mins
+        local_mins = which(diff(sign(diff(d[, measure_axis]))) == 2)
+        d[local_mins + 1, "local_optima"] = TRUE
+      } else {
+        # find local maxs
+        local_maxs = which(diff(sign(diff(d[, measure_axis]))) == -2)
+        d[local_maxs + 1, "local_optima"] = TRUE
+      }
+      plt = plt + geom_point(data = d, aes(color = local_optima)) +
+        scale_color_manual(values = c("TRUE" = "green", "FALSE" = "red"))
+    } else if (measure_used == 0) {
+      print("You asked for local optima but didn't specify a measure!")
+    }
+  }
+  
+  # pretty name changing
+  if (pretty.names) {
+    if (x_measure)
+      plt = plt + 
+        xlab(eval(as.name(stri_split_fixed(x.axis, ".test.mean")[[1]][1]))$name)
+    if (y_measure)
+      plt = plt + 
+        ylab(eval(as.name(stri_split_fixed(y.axis, ".test.mean")[[1]][1]))$name)
+  }
+  
+  if (!is.null(title))
+    plt = plt + ggtitle(title)
+  
+  return(plt)
 }
