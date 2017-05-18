cdc0192ad944faef8fa1dc7beba695da743d1b82
Minh Le <minh-anh.le@cdtm.de>
2017-05-18 11:43:59 +0200

    for now only work with the calibration with the sigmoid function (comment out the two mixture modeling version)

--- R/convertScoresToProbability.R
+++ R/convertScoresToProbability.R
@@ -64,83 +64,83 @@ convertingScoresToProbability = function(anomaly.score, parainit, method = "mixt
       }
     }
   }
-  else if (method == "mixture-model" ) {
-    if (is.null(p)) {
-      p = c(mu = 1, sigma = 1, lambda = 1, alpha = 0.5)
-      messagef("Starting parameter values are missing. For calibration using mixture modeling the default is: \n mu = 1, sigma = 1, lambda = 1, alpha = 1")
-    }
-    if (length(p) != 4) {
-      stop("Too little/many starting parameters. For calibration using sigmoid function starting values for parameter mu, sigma, lambda, alpha are needed.")
-    }
-
-    #p1 = mu, p2 = sigma, p3=lambda, p4 = alpha
-    gaussian = function(p) { 1 / (sqrt(2 * pi) * p[4]) * exp(-(f-p[1])^2 / (2 * p[2]^2)) }
-    exponential = function(p) { p[3] * exp (-p[3] * f) }
-    posteriori = function(p) { (p[4] * gaussian(p)) / (p[4] * gaussian(p) + (1-p[4]) * exponential(p)) }
-
-    while (loop) {
-      t = posteriori(p)
-      sumt = rep(1,length(t)) %*% t
-      tf = t(t) %*% f
-      mu = tf / sumt
-      sigma = (t(t) %*% (f - as.numeric(mu))^2) / sumt
-      lambda = sumt / tf
-      alpha = sumt / length(t)
-      pnew = c(mu, sigma, lambda, alpha)
-
-      if(any(is.na(pnew))) {
-        diff = FALSE
-        pnew = p
-      } else {
-        diff = sum(abs(pnew - p)) > 1e-3
-      }
-      if ( diff ) {
-        loop = TRUE
-        p = pnew
-      } else {
-        loop = FALSE
-        list$optim.solution = pnew
-        list$probability = posteriori(pnew)
-      }
-    }
-  } else {
-    if (is.null(p)) {
-      p = c(mu = 1, sigma = 1, lambda = 1, alpha = 0.5)
-      messagef("Starting parameter values are missing. For calibration using mixture modeling the default is: \n mu = 1, sigma = 1, lambda = 1, alpha = 1")
-    }
-    if (length(p) != 4) {
-      stop("Too little/many starting parameters. For calibration using sigmoid function starting values for parameter mu, sigma, lambda, alpha are needed.")
-    }
-
-    #p1 = mu, p2 = sigma, p3=lambda, p4 = alpha
-    gaussian = function(p) { 1 / (sqrt(2 * pi) * p[4]) * exp(-(f-p[1])^2 / (2 * p[2]^2)) }
-    exponential = function(p) { p[3] * exp (-p[3] * f) }
-    posteriori = function(p) { (p[4] * gaussian(p)) / (p[4] * gaussian(p) + (1-p[4]) * exponential(p)) }
-
-    while (loop) {
-      t = posteriori(p)
-      LL = function(p) { -sum(t * log(p[4] * gaussian(p)) + (1-t) * log((1-p[4]) * exponential(p))) }
-      gmu = function(p) { sum(t * ((f - p[1])^2 + 1) / p[2]^3) }
-      gsigma = function(p) { -sum(t * (f - p[1])^2 / p[2]^2) }
-      glambda = function(p) { -sum((1-t) * (1/p[3] - f)) }
-      galpha = function(p) { -sum( (t-p[4]) / (p[4]*(1-p[4]))) }
-      g = function(o) {
-        c(gmu(p), gsigma(p), glambda(p), galpha(p))
-      }
-      optim = trust.optim(p, fn = LL, gr = g,  method = "BFGS", control = list(report.level = 0))
-      pnew = optim$solution
-      diff = sum(abs(pnew - p))
-
-      if ( diff > 1e-2) {
-        loop = TRUE
-        p = pnew
-      } else {
-        loop = FALSE
-        list$optim.solution = pnew
-        list$probability = 1 / (1 + exp(-pnew[1] * f - pnew[2]))
-      }
-    }
-  }
+  # else if (method == "mixture-model" ) {
+  #   if (is.null(p)) {
+  #     p = c(mu = 1, sigma = 1, lambda = 1, alpha = 0.5)
+  #     messagef("Starting parameter values are missing. For calibration using mixture modeling the default is: \n mu = 1, sigma = 1, lambda = 1, alpha = 1")
+  #   }
+  #   if (length(p) != 4) {
+  #     stop("Too little/many starting parameters. For calibration using sigmoid function starting values for parameter mu, sigma, lambda, alpha are needed.")
+  #   }
+  #
+  #   #p1 = mu, p2 = sigma, p3=lambda, p4 = alpha
+  #   gaussian = function(p) { 1 / (sqrt(2 * pi) * p[4]) * exp(-(f-p[1])^2 / (2 * p[2]^2)) }
+  #   exponential = function(p) { p[3] * exp (-p[3] * f) }
+  #   posteriori = function(p) { (p[4] * gaussian(p)) / (p[4] * gaussian(p) + (1-p[4]) * exponential(p)) }
+  #
+  #   while (loop) {
+  #     t = posteriori(p)
+  #     sumt = rep(1,length(t)) %*% t
+  #     tf = t(t) %*% f
+  #     mu = tf / sumt
+  #     sigma = (t(t) %*% (f - as.numeric(mu))^2) / sumt
+  #     lambda = sumt / tf
+  #     alpha = sumt / length(t)
+  #     pnew = c(mu, sigma, lambda, alpha)
+  #
+  #     if(any(is.na(pnew))) {
+  #       diff = FALSE
+  #       pnew = p
+  #     } else {
+  #       diff = sum(abs(pnew - p)) > 1e-3
+  #     }
+  #     if ( diff ) {
+  #       loop = TRUE
+  #       p = pnew
+  #     } else {
+  #       loop = FALSE
+  #       list$optim.solution = pnew
+  #       list$probability = posteriori(pnew)
+  #     }
+  #   }
+  # } else {
+  #   if (is.null(p)) {
+  #     p = c(mu = 1, sigma = 1, lambda = 1, alpha = 0.5)
+  #     messagef("Starting parameter values are missing. For calibration using mixture modeling the default is: \n mu = 1, sigma = 1, lambda = 1, alpha = 1")
+  #   }
+  #   if (length(p) != 4) {
+  #     stop("Too little/many starting parameters. For calibration using sigmoid function starting values for parameter mu, sigma, lambda, alpha are needed.")
+  #   }
+  #
+  #   #p1 = mu, p2 = sigma, p3=lambda, p4 = alpha
+  #   gaussian = function(p) { 1 / (sqrt(2 * pi) * p[4]) * exp(-(f-p[1])^2 / (2 * p[2]^2)) }
+  #   exponential = function(p) { p[3] * exp (-p[3] * f) }
+  #   posteriori = function(p) { (p[4] * gaussian(p)) / (p[4] * gaussian(p) + (1-p[4]) * exponential(p)) }
+  #
+  #   while (loop) {
+  #     t = posteriori(p)
+  #     LL = function(p) { -sum(t * log(p[4] * gaussian(p)) + (1-t) * log((1-p[4]) * exponential(p))) }
+  #     gmu = function(p) { sum(t * ((f - p[1])^2 + 1) / p[2]^3) }
+  #     gsigma = function(p) { -sum(t * (f - p[1])^2 / p[2]^2) }
+  #     glambda = function(p) { -sum((1-t) * (1/p[3] - f)) }
+  #     galpha = function(p) { -sum( (t-p[4]) / (p[4]*(1-p[4]))) }
+  #     g = function(o) {
+  #       c(gmu(p), gsigma(p), glambda(p), galpha(p))
+  #     }
+  #     optim = trust.optim(p, fn = LL, gr = g,  method = "BFGS", control = list(report.level = 0))
+  #     pnew = optim$solution
+  #     diff = sum(abs(pnew - p))
+  #
+  #     if ( diff > 1e-2) {
+  #       loop = TRUE
+  #       p = pnew
+  #     } else {
+  #       loop = FALSE
+  #       list$optim.solution = pnew
+  #       list$probability = 1 / (1 + exp(-pnew[1] * f - pnew[2]))
+  #     }
+  #   }
+  # }
   return(list)
 }
 
