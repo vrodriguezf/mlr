4d3a05521327daa57a0d735c97f79e02d3ca09ad
Minh Le <minh-anh.le@cdtm.de>
2017-05-19 13:23:12 +0200

    threshold for oneclass only have one entry, unlike classif and multiclass, which have threshold for every class

--- R/setThreshold.R
+++ R/setThreshold.R
@@ -41,12 +41,17 @@ setThreshold = function(pred, threshold) {
   ttype = td$type
   levs = td$class.levels
   if (length(levs) == 2L && is.numeric(threshold) && length(threshold) == 1L) {
-    threshold = c(threshold, 1 - threshold)
-    names(threshold) = c(td$positive, td$negative)
+    if (pred$task.desc$type == "oneclass") {
+      names(threshold) = td$positive
+    } else {
+      threshold = c(threshold, 1 - threshold)
+      names(threshold) = c(td$positive, td$negative)
+    }
   }
 
-  if (length(threshold > 1L) && !setequal(levs, names(threshold)))
+  if ((pred$task.desc$type != "oneclass") && length(threshold > 1L) && !setequal(levs, names(threshold))) {
     stop("Threshold names must correspond to classes!")
+  }
   p = getPredictionProbabilities(pred, cl = levs)
   # resort so we have same order in threshold and p
   threshold = threshold[levs]
