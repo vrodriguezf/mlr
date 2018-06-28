67e953ff54539e5a15b5353cb1ab644331b8f052
pat-s <patrick.schratz@gmail.com>
2018-06-28 12:08:27 +0200

    see if man/ files are deployed

--- tic.R
+++ tic.R
@@ -23,7 +23,7 @@ get_stage("script") %>%
 
   get_stage("deploy") %>%
     add_code_step(system2("bash", args = c("inst/convert_to_ascii_news.sh"))) %>%
-    add_step(step_push_deploy(orphan = FALSE, branch = "master", commit_paths = c("NAMESPACE", "man/*", "NEWS")))
+    add_step(step_push_deploy(orphan = FALSE, branch = "tic-s4-debug", commit_paths = c("NAMESPACE", "man/*", "NEWS")))
 }
 
 if (Sys.getenv("TUTORIAL") == "HTML") {
