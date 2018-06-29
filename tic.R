270f30348d6b674dda418013e14a9ee857996065
Patrick Schratz <patrick.schratz@gmail.com>
2018-06-29 16:28:53 +0000

    Deploy from Travis build 11934 [ci skip]
    Build URL: https://travis-ci.org/mlr-org/mlr/builds/398289206
    Commit: 81fe1c0a3148558104b9887e487fe8c4fbabd634

--- tic.R
+++ tic.R
@@ -18,13 +18,12 @@ if (Sys.getenv("RCMDCHECK") == "TRUE") {
 
 get_stage("script") %>%
     add_code_step(devtools::install_github("r-lib/rcmdcheck")) %>%
-  add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
+    add_code_step(devtools::document()) %>%
     add_step(step_rcmdcheck(notes_are_errors = FALSE))
 
   get_stage("deploy") %>%
-    add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
     add_code_step(system2("bash", args = c("inst/convert_to_ascii_news.sh"))) %>%
-    add_step(step_push_deploy(orphan = FALSE, branch = "tic-s4-debug", commit_paths = c("NAMESPACE", "man/*", "NEWS")))
+    add_step(step_push_deploy(orphan = FALSE, branch = "master", commit_paths = c("NAMESPACE", "man/*", "NEWS")))
 }
 
 if (Sys.getenv("TUTORIAL") == "HTML") {
@@ -33,20 +32,17 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
   get_stage("install") %>%
+    add_code_step(if (length(find.package("magick", quiet = TRUE)) == 0) install.packages("magick")) %>% # favicon creation
     add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
     add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
 
   get_stage("before_deploy") %>%
-    add_step(step_setup_ssh()) %>%
-    add_code_step(system("sed -i -e '/^##/ s/#/' -e '/^###/ s/#/' -e '/^####/ s/#/' vignettes/tutorial/devel/*.Rmd")) %>%
-    add_code_step(system("sed -i -e '/^##/ s/#/' -e '/^###/ s/#/' -e '/^####/ s/#/' vignettes/tutorial/release/*.Rmd"))
+    add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_code_step(install.packages("magick")) %>% # favicon creation
     add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
     add_step(step_build_pkgdown()) %>%
     add_step(step_push_deploy(orphan = TRUE, path = "docs", branch = "gh-pages"))
@@ -57,8 +53,7 @@ if (Sys.getenv("TUTORIAL") == "HTML") {
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
   get_stage("install") %>%
     add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
@@ -84,8 +79,7 @@ if (Sys.getenv("TUTORIAL") == "PDFrelease") {
     add_code_step(if (length(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()]) > 0)
       install.packages(trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]])[!trimws(strsplit(Sys.getenv("WARMUPPKGS"), " ")[[1]]) %in% installed.packages()])) %>%
     add_code_step(system2("java", args = c("-cp", "$HOME/R/Library/RWekajars/java/weka.jar weka.core.WekaPackageManager",
-                                           "-install-package", "thirdparty/XMeans1.0.4.zip"))) %>%
-    add_code_step(devtools::install_deps(upgrade = TRUE, dependencies = TRUE))
+                                           "-install-package", "thirdparty/XMeans1.0.4.zip")))
 
   get_stage("install") %>%
     add_code_step(if (length(find.package("pander", quiet = TRUE)) == 0) install.packages("pander")) %>%
@@ -99,7 +93,7 @@ if (Sys.getenv("TUTORIAL") == "PDFrelease") {
     add_step(step_setup_ssh())
 
   get_stage("deploy") %>%
-    add_code_step(devtools::document(roclets=c('rd', 'collate', 'namespace'))) %>%
+    add_code_step(devtools::install_github("mlr-org/mlr")) %>%
     add_code_step(rmarkdown::render("vignettes/tutorial/release/pdf/_pdf_wrapper.Rmd")) %>%
     add_code_step(fs::file_move("vignettes/tutorial/release/pdf/_pdf_wrapper.pdf", "vignettes/tutorial/release/pdf/mlr-tutorial_release.pdf")) %>%
     add_step(step_push_deploy(orphan = FALSE, commit_paths = "vignettes/tutorial/release/pdf/mlr-tutorial_release.pdf", branch = "tutorial_pdf"))
